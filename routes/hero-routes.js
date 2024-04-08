7645862a931e267f1473b12ab35e0770ae3915af
Preston Bateman <prestonbbateman@gmail.com>
2024-04-08 18:18:10 -0500

    final hero fix

--- routes/hero-routes.js
+++ routes/hero-routes.js
@@ -5,8 +5,8 @@ const heroController = require("../controllers/hero-controller.js");
 heroRouter.get(
   "/",
   // #swagger.description = 'use this as the API key = Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N'
-  // #swagger.summary = 'get all comics'
-  // #swagger.tags = ['comics']
+  // #swagger.summary = 'get all heroes'
+  // #swagger.tags = ['heroes']
   heroController.getAllHeroes
 );
 
@@ -18,28 +18,28 @@ heroRouter.get(
     required: true,
     type: 'string'
    } */
-  // #swagger.summary = 'get a single comic by id'
-  // #swagger.tags = ['comics']
+  // #swagger.summary = 'get a single hero by id'
+  // #swagger.tags = ['heroes']
   heroController.getHeroById
 );
 
 heroRouter.post(
   "/",
-  // #swagger.summary = 'Add a new Comic',
-  // #swagger.tags = ['comics'],
+  // #swagger.summary = 'Add a new Hero',
+  // #swagger.tags = ['heroes'],
   /* #swagger.requestBody = {
       required: true,
       content: {
         "application/json": {
           schema: {
-            $ref: '#/components/schemas/Comic'
+            $ref: '#/components/schemas/Hero'
           }
         }
       }
     } */
   /* #swagger.responses[200] = {
-      description: 'Success: Comic was updated successfully.',
-      schema: { $ref: '#/components/schemas/Comic' }
+      description: 'Success: Hero was updated successfully.',
+      schema: { $ref: '#/components/schemas/Hero' }
     } */
   // #swagger.responses[400] = { description: 'Bad request: Data to update can not be empty!' }
   // #swagger.responses[404] = { description: 'Not found: Cannot update User with id. Maybe User was not found!' }
@@ -54,43 +54,43 @@ heroRouter.post(
 
 heroRouter.put(
   "/:id",
-  // #swagger.summary = 'update a Comic by id'
-  // #swagger.tags = ['comics']
+  // #swagger.summary = 'update a Hero by id'
+  // #swagger.tags = ['heroes']
   /* isAuthenticated, ******************************/
   heroController.updateHeroById
 );
 
 heroRouter.delete(
   "/:id",
-  // #swagger.summary = 'delete a single Comic by id'
-  // #swagger.tags = ['comics']
+  // #swagger.summary = 'delete a single Hero by id'
+  // #swagger.tags = ['heroes']
   /* isAuthenticated,   ******************************/
   heroController.deleteHeroById
 );
 
 heroRouter.delete(
   "/",
-  // #swagger.summary = 'delete all Comics'
-  // #swagger.tags = ['comics']
+  // #swagger.summary = 'delete all Heroes'
+  // #swagger.tags = ['heroes']
   /* isAuthenticated,  ******************************/
   heroController.deleteAllHeroes
 );
 
 heroRouter.patch(
   "/:id/toggleActiveStatus",
-  // #swagger.summary = 'toggle a Comic active status by id'
-  // #swagger.tags = ['comics']
+  // #swagger.summary = 'toggle a Hero active status by id'
+  // #swagger.tags = ['heroes']
   /* isAuthenticated,  ******************************/
   heroController.toggleHeroActiveStatus
 );
 
-// comicRouter.use(comicController.errorHandler)
+// heroRouter.use(heroController.errorHandler)
 
 module.exports = heroRouter;
 
-/* #swagger.parameters['newComic'] = {
+/* #swagger.parameters['newHero'] = {
     in: 'body',
-    description: 'Information for the new comic.',
+    description: 'Information for the new hero.',
     required: true,
     schema: {
       type: 'object',
